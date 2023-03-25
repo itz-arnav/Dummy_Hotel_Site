a4fd9c01bdc839ed8f5ba6b1ab9c57a53bf0f1e9
Arnab Jha <64909912+itz-arnav@users.noreply.github.com>
2023-03-25 16:08:31 +0530

    pushed

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,47 +1,7 @@
-import React from "react";
-import css from "./styles/Login.module.css";
 
 const Login = () => {
 	return (
-		<>
-			<div className={css.outerContainer}>
-				<div className={css.innerContainer}>
-					<div className={css.imageContainer}>
-						<img
-							className={css.image}
-							src="https://images.unsplash.com/photo-1562774053-701939374585?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1186&q=80"
-							alt=""
-						/>
-					</div>
-					<div className={css.loginSection}>
-						<h1>Login Here</h1>
-						<form>
-							<div>
-								<label htmlFor="username">Username:</label>
-								<input type="text" id="username" />
-							</div>
-							<div>
-								<label htmlFor="password">Password:</label>
-								<input type="password" id="password" />
-							</div>
-							<button type="submit">Login</button>
-							<div>
-								<label htmlFor="remember-me">Remember me:</label>
-								<input type="checkbox" id="remember-me" />
-							</div>
-						</form>
-					</div>
-					<div className={css.footerPart}>
-						<a href="" className={css.register}>
-							Register
-						</a>
-						<a href="" className={css.forgotPass}>
-							Forgot Password?
-						</a>
-					</div>
-				</div>
-			</div>
-		</>
+		<h1>Hi</h1>
 	);
 };
 
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,11 +1,66 @@
-import React from 'react'
+import css from "./Register.module.css";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
 
 const Register = () => {
-  return (
-    <div>
-      <h1 className = {css.container}>Register</h1>
-    </div>
-  )
-}
+	return (
+		<div className={css.container}>
+			<div className={css.innerContainer}>
+				<div className={css.imageSection}>
+					<img
+						src="https://images.unsplash.com/photo-1602330041000-4b8119482edf?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1460&q=80"
+						alt=""
+					/>
+				</div>
 
-export default Register
+        <div className={css.signupContainer}>
+        <div className={css.signupSection}>
+					<div className={css.textSection}>
+						<h1>Sign Up</h1>
+						<p>Please fill in the form to create an account</p>
+					</div>
+          <hr />
+					<Form>
+						<Form.Group className="mb-3" controlId="formBasicEmail">
+							<Form.Label className="fw-bold">Email address</Form.Label>
+							<Form.Control type="email" placeholder="Enter email" className = {css.greyRegion} />
+						</Form.Group>
+
+						<Form.Group className="mb-3" controlId="formBasicPassword">
+							<Form.Label className="fw-bold">Password</Form.Label>
+							<Form.Control type="password" placeholder="Password" className = {css.greyRegion}/>
+						</Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicPassword"> 
+							<Form.Label className="fw-bold">Repeat Password</Form.Label>
+							<Form.Control type="password" placeholder="Password" className = {css.greyRegion}/>
+						</Form.Group>
+
+						<Form.Group className="mb-3" controlId="formBasicCheckbox">
+							<Form.Check type="checkbox" label="Remember Me" />
+						</Form.Group>
+
+            <p>By creating an account, you agree to our <a href="">Terms & Privacy.</a></p>
+
+            <div className={css.buttonSection}>
+            <Button variant="danger" type="submit" className={css.btn}>
+							Cancel
+						</Button>
+            <Button variant="success" type="submit" className={css.btn}>
+							Sign Up 
+						</Button>
+            </div>
+					</Form>
+				</div>
+        </div>
+				
+
+        <div className={css.copyrightSection}>
+          <h1>Copyright ©2023 All Cheems Rights Limited | Design Group: 4</h1>
+        </div>
+			</div>
+		</div>
+	);
+};
+
+export default Register;
